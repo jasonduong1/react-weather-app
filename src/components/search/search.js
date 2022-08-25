91ba2f17caa77dd691b875a00ca4c300358410e7
jason <jduong89@gmail.com>
2022-08-25 16:37:23 -0700

    adds return value for city lat and long plus label for city name and country code

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,10 +6,18 @@ const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
   const loadOptions = (inputValue) => {
-    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+    return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions)
       .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => console.error(err));
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
   };
 
   const handleOnChange = (searchData) => {
