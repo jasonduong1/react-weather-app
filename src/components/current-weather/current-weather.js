36f6ce2fc3c7d6a1c92b56d59bae2f767f2a63cc
jason <jduong89@gmail.com>
2022-08-26 16:02:47 -0700

    adds static info for testing

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,7 +1,15 @@
 import "./current-weather.css";
 
 const CurrentWeather = () => {
-  return "hello";
+  return (
+    <div className="weather">
+      <div className="top">
+        <p className="city">Oakland</p>
+        <p className="weather-description"> Sunny</p>
+      </div>
+      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+    </div>
+  );
 };
 
 export default CurrentWeather;
