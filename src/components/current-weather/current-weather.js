64bb37724e578d2072374f5024c164f6117c3557
jason <jduong89@gmail.com>
2022-08-26 16:34:55 -0700

    adds details portion of current weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,10 +4,36 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Oakland</p>
-        <p className="weather-description"> Sunny</p>
+        <div>
+          <p className="city">Oakland</p>
+          <p className="weather-description"> Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      <div className="bottom">
+        <p className="temperature">80°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">82°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">10 MPH</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">20%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">10 pascal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
