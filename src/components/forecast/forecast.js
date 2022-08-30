4d3d8526ef43cd78668b78e583fec30936592752
jason <jduong89@gmail.com>
2022-08-30 16:55:57 -0700

    imports forecast css

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -5,6 +5,7 @@ import {
   AccordionItemHeading,
   AccordionItemPanel,
 } from "react-accessible-accordion";
+import "./forecast.css";
 
 const WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
