0a11f0b418bf6a91a16f77dc9401300352f6c8e7
jason <jduong89@gmail.com>
2022-08-30 16:54:27 -0700

    adds forecast logic

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,5 +1,43 @@
-const Forecast = () => {
-  return "hello";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+  console.log(forecastDays);
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">{item.weather[0].description}</label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°F / {Math.round(item.main.temp_max)}°F
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
 };
 
 export default Forecast;
